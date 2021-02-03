7aebff1c6416b2a081fefc9ec5d40e3ccf087680
anorth <445306+anorth@users.noreply.github.com>
2021-02-03 17:01:17 +1100

    Writer handles already-encoded data, or ipld.Node

--- pkg/dar/metadata.go
+++ pkg/dar/metadata.go
@@ -1,23 +1,45 @@
 package dar
 
 import (
+	"io"
+
 	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/codec/dagcbor"
 	basicnode "github.com/ipld/go-ipld-prime/node/basic"
 )
 
 // DAR metadata object.
-// The metadata is an IPLD list, containing:
-// - 0: whether there is an index or not (bool)
+type archiveMeta struct {
+	hasIndex bool
+}
 
+func (m *archiveMeta) Serialize(wr io.Writer) error {
+	if node, err := m.ToNode(); err != nil {
+		return err
+	} else if err := dagcbor.Encoder(node, wr); err != nil {
+		return err
+	}
+	return nil
+}
 
-// Builds an IPLD list containing DAR metadata.
-func buildMetadata(hasIndex bool) (ipld.Node, error) {
+func (m *archiveMeta) Deserialize(rd io.Reader) error {
+	bld := basicnode.Prototype.List.NewBuilder()
+	if err := dagcbor.Decoder(bld, rd); err != nil {
+		return err
+	}
+	if err := m.FromNode(bld.Build()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *archiveMeta) ToNode() (ipld.Node, error) {
 	bld := basicnode.Prototype.List.NewBuilder()
 	listAssembler, err := bld.BeginList(1)
 	if err != nil {
 		return nil, err
 	}
-	err = listAssembler.AssembleValue().AssignBool(hasIndex)
+	err = listAssembler.AssembleValue().AssignBool(m.hasIndex)
 	if err != nil {
 		return nil, err
 	}
@@ -25,7 +47,17 @@ func buildMetadata(hasIndex bool) (ipld.Node, error) {
 	if err != nil {
 		return nil, err
 	}
-	metadataNode := bld.Build()
-	return metadataNode, nil
+	return bld.Build(), nil
 }
 
+func (m *archiveMeta) FromNode(node ipld.Node) error {
+	hasIndexNode, err := node.LookupByIndex(0)
+	if err != nil {
+		return err
+	}
+	m.hasIndex, err = hasIndexNode.AsBool()
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- pkg/dar/writer.go
+++ pkg/dar/writer.go
@@ -2,6 +2,7 @@ package dar
 
 import (
 	"bytes"
+	"context"
 	"encoding/binary"
 	"hash"
 	"io"
@@ -9,25 +10,38 @@ import (
 
 	cid "github.com/ipfs/go-cid"
 	ipld "github.com/ipld/go-ipld-prime"
-	"github.com/ipld/go-ipld-prime/codec"
-	"github.com/ipld/go-ipld-prime/codec/dagcbor"
 	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
+	basicnode "github.com/ipld/go-ipld-prime/node/basic"
 	"github.com/ipld/go-ipld-prime/traversal"
-	multihash "github.com/multiformats/go-multihash"
-	"golang.org/x/crypto/sha3"
 	"golang.org/x/xerrors"
 )
 
 // TODO:
 // - wrap errors
 // - handle RAW CIDs: no block data or indirection
-// - spill allCids and offsets to disk when too large for memory
+// - spill allCids and blocksOffset to disk when too large for memory
 
 ///// Writer
 
+// Writer writes IPLD blocks to an archive file.
+//
+// DAGs are written in strict depth-first order from some root. The writer enforces this, keeping track
+// of the links seen in nodes written so far, and accepting subsequent blocks only in accordance with a
+// depth-first traversal.
+// A DAG need not be complete: the caller need not provide a block for every link, but those that are provided
+// must be in the expected order.
+// Blocks are deduplicated internally. The same block may be provided multiple times, assuming it is linked
+// multiple times, and should generally be provided each time (but will only be encoded and written once).
+//
+// An archive may contain multiple DAGs. The DAG roots must be distinct. A block that has been written as
+// part of one DAG may not subsequently be used as a root. Thus, to the extent that any DAGs are contained
+// within another, they must be provided in depth-first order.
+//
+// The archive writer optionally writes an index at the end of the file, enabling subsequent rapid look-up
+// of any block by a Reader.
 type Writer struct {
 	// Whether to accumulate and write an index at the end of the archive.
-	index bool
+	makeIndex bool
 	// Stream to which archive is written.
 	writer countingWriter
 	// Optional function to call when the archive is closed.
@@ -54,49 +68,95 @@ type Writer struct {
 	expectedBlocks []cidlink.Link
 }
 
-func NewWriter(index bool) *Writer {
-	// Ensure the function and code match.
-	// The multihash library doesn't provide pre-registered table for streaming hash functions.
-	return &Writer{
-		index:      index,
-		offsets:    map[cid.Cid]int64{},
-		digest:     sha3.New512(),
-		digestCode: multihash.SHA2_256,
-	}
-}
-
-// Creates or truncates a file at path and writes the header.
+// Creates or truncates an archive file at a path, and writes the header.
 // The file will be closed when archive is closed.
-func (wr *Writer) OpenArchiveFile(path string) error {
-	if wr.writer.underlying != nil {
-		return xerrors.Errorf("already opened")
-	}
+func NewFileWriter(path string, index bool) (*Writer, error) {
 	f, err := os.Create(path)
 	if err != nil {
-		return err
+		return nil, err
+	}
+	wr, err := NewWriter(f, index)
+	if err != nil {
+		return nil, err
 	}
 	wr.closer = f
-	return wr.OpenArchive(f)
+	return wr, err
 }
 
-// Initializes an archive writer with a stream, writing the header to the stream and preparing to write blocks.
-func (wr *Writer) OpenArchive(iow io.Writer) error {
-	wr.writer = countingWriter{
-		underlying:   iow,
-		bytesWritten: 0,
+// Initializes an archive writer with a stream, writing the header and preparing to write blocks.
+func NewWriter(iow io.Writer, index bool) (*Writer, error) {
+	wr := &Writer{
+		makeIndex: index,
+		writer: countingWriter{
+			underlying:   iow,
+			bytesWritten: 0,
+		},
+		offsets:    map[cid.Cid]int64{},
+		digest:     digestFactory(),
+		digestCode: digestMultiHashType,
 	}
+
 	if err := wr.writeHeader(); err != nil {
-		return err
+		return nil, err
 	}
 	wr.blocksOffset = wr.writer.bytesWritten
-	return nil
+	return wr, nil
+}
+
+// Appends a new DAG root block to the archive.
+// Any expected blocks from a prior DAG are forgotten and may no longer be appended, unless they are contained in.
+// of this or a subsequent DAG.
+// Returns the links discovered in this node, in order, which are eligible to be
+// written next.
+func (wr *Writer) BeginDAG(ctx context.Context, rootBlock ipld.Node, enc cid.Prefix) (cidlink.Link, []cidlink.Link, error) {
+	if err := wr.checkOpen(); err != nil {
+		return cidlink.Link{}, nil, err
+	}
+
+	// Write the block.
+	link, links, err := wr.appendBlock(ctx, rootBlock, enc, true)
+	if err != nil {
+		return cidlink.Link{}, nil, err
+	}
+
+	// Empty the stack of expected blocks from the prior DAG (if any).
+	wr.expectedBlocks = nil
+
+	return link, links, nil
+}
+
+// Appends a block to the archive.
+// Returns the links discovered in this node, in order, which are eligible to be written next.
+// Links returned here must be provided before any links returned from previous calls
+// (i.e. a depth-first traversal).
+// TODO: more docs about this
+// - if block has been written previously, no links will be returned. decision about what sub-dag to write
+//   must have been made the first time.
+func (wr *Writer) AppendBlock(ctx context.Context, block ipld.Node, enc cid.Prefix) (cidlink.Link, []cidlink.Link, error) {
+	if err := wr.checkOpen(); err != nil {
+		return cidlink.Link{}, nil, err
+	}
+
+	return wr.appendBlock(ctx, block, enc, false)
+}
+
+func (wr *Writer) AppendRawBlock(ctx context.Context, link cidlink.Link, data []byte) ([]cidlink.Link, error) {
+	if err := wr.checkOpen(); err != nil {
+		return nil, err
+	}
+
+	return wr.appendRawBlock(ctx, link, data, false)
 }
 
 // Writes the index (if configured) and trailer to the stream.
-func (wr *Writer) CloseArchive() error {
+func (wr *Writer) Finish() error {
+	if err := wr.checkOpen(); err != nil {
+		return err
+	}
+
 	// Optionally write the index
 	indexOffset := int64(-1)
-	if wr.index {
+	if wr.makeIndex {
 		indexOffset = wr.writer.bytesWritten
 		if err := wr.writeIndex(); err != nil {
 			return err
@@ -112,39 +172,92 @@ func (wr *Writer) CloseArchive() error {
 			return err
 		}
 	}
+
+	wr.writer.underlying = nil
+	wr.closer = nil
 	return nil
 }
 
-// Appends a new root block to the archive.
-// Returns the links discovered in this node, in order, which are eligible to be
-// written next.
-func (wr *Writer) BeginDAG(rootCID cid.Cid, rootBlock ipld.Node, enc codec.Encoder) ([]ipld.Link, error) {
-	// Require the new root to be distinct from any block already seen.
-	// Note that this rejects an attempt to define a block already written as a root of a new DAG.
-	// If the DAGs had been written in the opposite order, both could succeed (and the child -DAG would be
-	// referenced from the parent DAG).
-	if _, found := wr.offsets[rootCID]; found {
-		return nil, xerrors.Errorf("block %v already written", rootCID)
+func (wr *Writer) checkOpen() error {
+	if wr.writer.underlying == nil {
+		return xerrors.Errorf("writer is closed")
 	}
-
-	// Empty the stack of expected blocks from the prior DAG (if any).
-	wr.expectedBlocks = nil
-
-	// Record the root CID.
-	wr.roots = append(wr.roots, rootCID)
-
-	// Write the block.
-	return wr.appendBlock(rootCID, rootBlock, enc)
+	return nil
 }
 
-// Appends a block to the archive.
-// Returns the links discovered in this node, in order, which are eligible to be written next.
-// Links returned here must be provided before any links returned from previous calls
-// (i.e. a depth-first traversal).
-// TODO: more docs about this
-// - if block has been written previously, no links will be returned. decision about what sub-dag to write
-//   must have been made the first time.
-func (wr *Writer) AppendBlock(c cid.Cid, block ipld.Node, enc codec.Encoder) ([]ipld.Link, error) {
+//
+// Internal implementation.
+//
+
+func (wr *Writer) writeHeader() error {
+	// TODO: consider moving this magic inside the DAG-CBOR object, as first element in the list.
+	// Write magic bytes for sniffing.
+	_, err := wr.writer.Write([]byte(magic))
+	if err != nil {
+		return err
+	}
+
+	// Write metadata object as DAG-CBOR
+	meta := archiveMeta{hasIndex: wr.makeIndex}
+	if err := meta.Serialize(&wr.writer); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (wr *Writer) appendBlock(ctx context.Context, block ipld.Node, enc cid.Prefix, root bool) (cidlink.Link, []cidlink.Link, error) {
+	inLink, data, err := encodeBlock(ctx, block, enc)
+	if err != nil {
+		return cidlink.Link{}, nil, err
+	}
+
+	if err := wr.receiveBlockData(inLink.Cid, data, root); err != nil {
+		return cidlink.Link{}, nil, err
+	}
+
+	// Push links to children onto the stack.
+	outLinks, err := wr.pushLinks(block)
+	if err != nil {
+		return cidlink.Link{}, nil, err
+	}
+
+	return inLink, outLinks, nil
+}
+
+func (wr *Writer) appendRawBlock(ctx context.Context, lnk cidlink.Link, data []byte, root bool) ([]cidlink.Link, error) {
+	block, err := decodeBlock(ctx, lnk, data)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := wr.receiveBlockData(lnk.Cid, data, root); err != nil {
+		return nil, err
+	}
+
+	// Push links to children onto the stack.
+	outLinks, err := wr.pushLinks(block)
+	if err != nil {
+		return nil, err
+	}
+
+	return outLinks, nil
+}
+
+func (wr *Writer) receiveBlockData(c cid.Cid, data []byte, root bool) error {
+	// For root blocks, check uniqueness before writing (rather than write a pointer).
+	if root {
+		// Require a new root to be distinct from any block already seen.
+		// Note that this rejects an attempt to define a block already written as a root of a new DAG.
+		// If the DAGs had been written in the opposite order, both could succeed (and the child DAG would be
+		// referenced from the parent DAG).
+		if _, found := wr.offsets[c]; found {
+			return xerrors.Errorf("block %v proposed as root already written", c)
+		}
+
+		// Record the root CID.
+		wr.roots = append(wr.roots, c)
+	}
+
 	// Pop expected blocks from the stack until we find this one.
 	for len(wr.expectedBlocks) > 0 {
 		top := len(wr.expectedBlocks) - 1
@@ -157,80 +270,46 @@ func (wr *Writer) AppendBlock(c cid.Cid, block ipld.Node, enc codec.Encoder) ([]
 
 	// TODO: can we restore state so that an error here is recoverable?
 	if len(wr.expectedBlocks) == 0 {
-		return nil, xerrors.Errorf("unexpected cid %v in depth-first traversal, not linked from ancestor block", c)
+		return xerrors.Errorf("unexpected cid %v in depth-first traversal, not linked from ancestor block", c)
 	}
 
-	return wr.appendBlock(c, block, enc)
-}
-
-//
-// Internal implementation.
-//
-
-func (wr *Writer) writeHeader() error {
-	// TODO: consider moving this magic inside the DAG-CBOR object, as first element in the list.
-	// Write magic bytes for sniffing.
-	_, err := wr.writer.Write([]byte("dar\x01"))
-	if err != nil {
-		return err
-	}
-
-	// Write metadata object as DAG-CBOR
-	if metadata, err := buildMetadata(wr.index); err != nil {
-		return err
-	} else if err := dagcbor.Encoder(metadata, &wr.writer); err != nil {
+	// Write block data to the stream.
+	if err := wr.writeBlock(c, data); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (wr *Writer) appendBlock(c cid.Cid, block ipld.Node, enc codec.Encoder) ([]ipld.Link, error) {
+func (wr *Writer) writeBlock(c cid.Cid, data []byte) error {
 	// If the block (and hence its sub-DAG) has already been written in this archive,
 	// insert a pointer to it.
 	if offset, found := wr.offsets[c]; found {
 		// Write the absolute offset at which the block is already written, negated.
 		_, err := wr.writeVarint(-offset)
-		return nil, err
+		return err
 	}
 
 	// Record the block CID and offset at which it will be written.
 	wr.allCids = append(wr.allCids, c)
 	wr.offsets[c] = wr.writer.bytesWritten
 
-	// Encode to a buffer, so we can calculate the serialized length.
-	// TODO: avoid this buffer when the underlying node already knows its serialised length.
-	var blockBuf bytes.Buffer
-	if err := enc(block, &blockBuf); err != nil {
-		return nil, err
-	}
-	// TODO: optionally verify the CID
-	// Should be be computing the CID here, or accepting a function argument?
-
 	// Write length and block data.
-	if _, err := wr.writeVarint(int64(blockBuf.Len())); err != nil {
-		return nil, err
+	if _, err := wr.writeVarint(int64(len(data))); err != nil {
+		return err
 	}
 	// Write block data.
-	if _, err := wr.writer.Write(blockBuf.Bytes()); err != nil {
-		return nil, err
+	if _, err := wr.writer.Write(data); err != nil {
+		return err
 	}
-
 	// Accumulate digest
 	if _, err := wr.digest.Write(c.Bytes()); err != nil {
-		return nil, err
+		return err
 	}
-
-	// Push links to children onto the stack.
-	links, err := wr.pushLinks(c, block)
-	if err != nil {
-		return nil, err
-	}
-
-	return links, nil
+	return nil
 }
 
 func (wr *Writer) writeIndex() error {
-	panic("not implemented")
+	panic("not yet implemented")
 }
 
 // Writes the archive trailer to the stream.
@@ -247,7 +326,7 @@ func (wr *Writer) writeTrailer(indexOffset int64) error {
 		}
 	}
 
-	// Write digest of block CIDs (fixed width)
+	// Write digest of block CIDs (fixed width, may depend on version)
 	if _, err := wr.writer.Write(wr.digest.Sum([]byte{})); err != nil {
 		return err
 	}
@@ -264,38 +343,91 @@ func (wr *Writer) writeTrailer(indexOffset int64) error {
 	return nil
 }
 
-// Writes a varint to the underlying stream.
+// Writes a varint to the stream.
 func (wr *Writer) writeVarint(i int64) (int, error) {
 	buf := make([]byte, binary.MaxVarintLen64)
 	n := binary.PutVarint(buf, i)
 	return wr.writer.Write(buf[:n])
 }
 
-// Writes an int64 to the underlying stream (always 8 bytes).
+// Writes an int64 to the stream (always 8 bytes).
 func (wr *Writer) writeInt64(i int64) error {
 	return binary.Write(&wr.writer, binary.BigEndian, i)
 }
 
-// Extracts links from a block and pushes them onto the the stack of expected blocks.
+// Pushes pushes them onto the the stack of expected blocks.
 // The new links will appear at the top of the stack, in traversal order.
-func (wr *Writer) pushLinks(c cid.Cid, block ipld.Node) ([]ipld.Link, error) {
+// Returns the links as CID links, in the same order they were provided.
+func (wr *Writer) pushLinks(block ipld.Node) ([]cidlink.Link, error) {
 	links, err := traversal.SelectLinks(block)
 	if err != nil {
 		return nil, err
 	}
-	for _, lnk := range links {
+	cidLinks := make([]cidlink.Link, len(links))
+
+	// Traverse links backwards to push onto the stack.
+	for i := len(links) - 1; i >= 0; i-- {
+		lnk := links[i]
 		cidLnk, ok := lnk.(cidlink.Link)
 		if !ok {
-			return nil, xerrors.Errorf("in block %v expected CID link, got %v", c, lnk)
+			return nil, xerrors.Errorf("expected CID link, got %v", lnk)
 		}
+		cidLinks[i] = cidLnk
 		wr.expectedBlocks = append(wr.expectedBlocks, cidLnk)
 	}
-	return links, nil
+	return cidLinks, nil
 }
 
 //
 // Helpers
 //
+
+// Encodes an IPLD node according to the spec given by a CID prefix.
+// Returns the CID and encoded data buffer.
+func encodeBlock(ctx context.Context, block ipld.Node, encspec cid.Prefix) (cidlink.Link, []byte, error) {
+	// It's kinda weird to go through the link builder to encode the data and then create a CID for it,
+	// but the multicodec tables are not exported so this is the only way that avoids duplicating them.
+	// It's much more complex than what we're really trying to do.
+	encoder := cidlink.LinkBuilder{
+		Prefix: encspec,
+	}
+
+	// Serialize into a buffer first because we need to calculate the length before writing
+	// the block data to stream. This buffer is extra unfortunate because there's also an unwanted
+	// buffer in LinkBuilder, and we could at least have hoped to use the same buffer for calculating
+	// both the hash and the length :'-(
+	var dataBuf bytes.Buffer
+	storer := func(lnkCtx ipld.LinkContext) (io.Writer, ipld.StoreCommitter, error) {
+		return &dataBuf, nilCommitter, nil
+	}
+
+	lnk, err := encoder.Build(ctx, ipld.LinkContext{}, block, storer)
+	if err != nil {
+		return cidlink.Link{}, nil, err
+	}
+	return lnk.(cidlink.Link), dataBuf.Bytes(), nil
+}
+
+// Decodes an IPLD code according to the spec give by a CID link.
+// Returns the inflated node.
+func decodeBlock(ctx context.Context, lnk cidlink.Link, data []byte) (ipld.Node, error) {
+	// Using the link loader for this is weird and inefficient, but the codec tables are not exposed directly.
+	// See analogous comment about encoding.
+	loader := func(loadLnk ipld.Link, lnkCtx ipld.LinkContext) (io.Reader, error) {
+		if loadLnk != lnk {
+			return nil, xerrors.Errorf("can't load %v", loadLnk)
+		}
+		return bytes.NewBuffer(data), nil
+	}
+	// Inflating the entire structure just to be able to select the links out of it is a big waste of allocations,
+	// but the boilerplate to do so with a custom NodeAssembler is too much to stomach.
+	builder := basicnode.Prototype.Any.NewBuilder()
+	// Loading the link validates that the content matches the CID.
+	if err := lnk.Load(ctx, ipld.LinkContext{}, builder, loader); err != nil {
+		return nil, err
+	}
+	return builder.Build(), nil
+}
 
 // Wraps a writer to count the bytes written.
 // This structure is necessary to count bytes when passing a writer to another
@@ -314,3 +446,5 @@ func (c *countingWriter) Write(bs []byte) (n int, err error) {
 	}
 	return n, err
 }
+
+var nilCommitter ipld.StoreCommitter = func(link ipld.Link) error { return nil }
